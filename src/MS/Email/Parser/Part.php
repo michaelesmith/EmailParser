806f7d3e95762c9267b7b60ad4b0020c7178274f
SebSept <sebastienmonterisi@yahoo.fr>
2015-01-15 14:24:30 +0100

    Revert "break Part::getDecodedContent() : tests still ok !"
    This reverts commit 0c79b28c32502165417a3b6284aad1b944260ca4.

--- src/MS/Email/Parser/Part.php
+++ src/MS/Email/Parser/Part.php
@@ -35,9 +35,7 @@ class Part
                 $content = base64_decode($this->content);
                 break;
             case 'quoted-printable':
-                // break !!
-                $content = $this->content;
-//                $content = quoted_printable_decode($this->content);
+                $content = quoted_printable_decode($this->content);
                 break;
             default:
                 throw new \RuntimeException(sprintf('unknown encoding type "%s"', $this->encoding));
